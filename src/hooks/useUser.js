1ecdcf400a5d32cf95d867310e507f4b8d9a684b
IvannEmmanuel <F0dG1ngD13@gmail.com>
2025-09-19 18:50:55 +0800

    refactor: added a saveUser and removeUser

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -2,13 +2,25 @@ import { useState, useEffect } from "react";
 
 export function useUser() {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const storedUser = localStorage.getItem("loggedInUser");
     if (storedUser) {
       setUser(JSON.parse(storedUser));
     }
+    setLoading(false);
   }, []);
 
-  return user;
+  const saveUser = (userData) => {
+    localStorage.setItem("loggedInUser", JSON.stringify(userData));
+    setUser(userData);
+  };
+
+  const removeUser = () => {
+    localStorage.removeItem("loggedInUser");
+    setUser(null);
+  };
+
+  return { user, saveUser, removeUser, loading };
 }
